9a67b01ccf9906036d98327e88af4c9e105d5281
jackgeorge11 <jackkgeorgee@gmail.com>
2021-12-05 20:56:11 +0000

    started pug templating and got bare baones app.js files working

---
+++ app.js
@@ -0,0 +1,22 @@
+// Import express.js
+const express = require("express");
+
+// Create express app
+var app = express();
+
+// Declare file structure for pug
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+// Add static files location
+app.use(express.static("static"));
+
+// Routes
+
+app.get('/', (req, res) => {
+    res.render("index");
+});
+
+app.listen(3000,function(){
+    console.log(`Server running at http://127.0.0.1:3000/`);
+});
